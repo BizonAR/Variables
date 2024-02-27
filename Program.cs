c962efeef8875789497c6238ebf7a2c70767154a
mikaa2001 <mikaa2001@gmail.com>
2024-02-27 23:54:23 +0500

    Variables

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int age = 25;
+			double height = 175.5;
+			string firstName = "Andrey";
+			char gender = 'M';
+			bool isStudent = true;
+
+			byte numberOfCoursec = 5;
+			short population = 30000;
+			long phonenumber = 1234567890;
+			float weight = 70.3f;
+			decimal salary = 50000.75m;
+		}
+	}
+}
